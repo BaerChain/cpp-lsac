d84db01679d71bdd26d993ecf2830a711d497af7
woodbaer <woodbaer@baerchain.net>
2018-10-02 19:51:14 -0600

    Virtual machine and switch to Boost's big num type for 256-bit ints.

--- Common.h
+++ Common.h
@@ -5,22 +5,30 @@
 #include <cstdint>
 #include <boost/multiprecision/cpp_int.hpp>
 #include "foreign.h"
-#include "uint256_t.h"
 
 namespace eth
 {
 
 using byte = uint8_t;
-using bytes = vector<byte>;
+using bytes = std::vector<byte>;
 
 using fBytes = foreign<byte>;
 using fConstBytes = foreign<byte const>;
 
-using bigint = boost::multiprecision::cpp_int;
-using u256 = uint256_t;
+using bigint = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<>>;
+using u256 =  boost::multiprecision::number<boost::multiprecision::cpp_int_backend<256, 256, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>>;
+using s256 =  boost::multiprecision::number<boost::multiprecision::cpp_int_backend<256, 256, boost::multiprecision::signed_magnitude, boost::multiprecision::unchecked, void>>;
 using uint = uint64_t;
 using sint = int64_t;
 
 template <class _T> std::string toString(_T const& _t) { std::ostringstream o; o << _t; return o.str(); }
+
+template <class _T> inline std::string asHex(_T const& _data)
+{
+	std::ostringstream ret;
+	for (auto i: _data)
+		ret << std::hex << std::setfill('0') << std::setw(2) << (int)i;
+	return ret.str();
+}
 
 }
--- RLP.h
+++ RLP.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <iomanip>
 #include "foreign.h"
 #include "Common.h"
 
@@ -24,6 +25,9 @@ public:
 
 	/// Construct a node of value given in the bytes.
 	explicit RLP(fConstBytes _d): m_data(_d) {}
+
+	/// Construct a node of value given in the bytes.
+	explicit RLP(bytes const& _d): m_data(const_cast<bytes*>(&_d)) {}	// a bit horrible, but we know we won't be altering the data. TODO: allow vector<T> const* to be passed to foreign<T const>.
 
 	/// Construct a node to read RLP data in the bytes given.
 	RLP(byte const* _b, uint _s): m_data(fConstBytes(_b, _s)) {}
@@ -64,6 +68,26 @@ public:
 	/// @returns the number of characters in the string, or zero if it isn't a string.
 	uint stringSize() const { return isString() ? items() : 0; }
 
+	bool operator==(char const* _s) const { return isString() && toString() == _s; }
+	bool operator==(std::string const& _s) const { return isString() && toString() == _s; }
+	bool operator==(uint const& _i) const { return toSlimInt() == _i; }
+	bool operator==(u256 const& _i) const { return toFatInt() == _i; }
+	bool operator==(bigint const& _i) const { return toBigInt() == _i; }
+	RLP operator[](uint _i) const
+	{
+		if (!isList() || itemCount() <= _i)
+			return RLP();
+		fConstBytes d = payload();
+		for (uint64_t i = 0; i < _i; ++i, d = d.cropped(RLP(d).size())) {}
+		return RLP(d);
+	}
+
+	explicit operator std::string() const { return toString(); }
+	explicit operator RLPs() const { return toList(); }
+	explicit operator uint() const { return toSlimInt(); }
+	explicit operator u256() const { return toFatInt(); }
+	explicit operator bigint() const { return toBigInt(); }
+
 	std::string toString() const
 	{
 		if (!isString())
@@ -78,9 +102,10 @@ public:
 		if (isDirectValueInt())
 			return m_data[0];
 		_T ret = 0;
-		auto s = intSize();
+		auto s = intSize() - intLengthSize();
+		uint o = intLengthSize() + 1;
 		for (uint i = 0; i < s; ++i)
-			ret = (ret << 8) | m_data[i + 1];
+			ret = (ret << 8) | m_data[i + o];
 		return ret;
 	}
 
@@ -118,6 +143,8 @@ private:
 
 	uint size() const
 	{
+		if (isNull())
+			return 0;
 		if (isInt())
 			return 1 + intSize();
 		if (isString())
@@ -156,6 +183,138 @@ private:
 	fConstBytes m_data;
 };
 
+struct RLPList { RLPList(uint _count): count(_count) {} uint count; };
+
+class RLPStream
+{
+public:
+	RLPStream() {}
+
+	void append(uint _s) { appendNumeric(_s); }
+	void append(u256 _s) { appendNumeric(_s); }
+	void append(bigint _s) { appendNumeric(_s); }
+
+	void append(std::string const& _s)
+	{
+		if (_s.size() < 0x38)
+			m_out.push_back(_s.size() | 0x40);
+		else
+			pushCount(_s.size(), 0x40);
+		uint os = m_out.size();
+		m_out.resize(os + _s.size());
+		memcpy(m_out.data() + os, _s.data(), _s.size());
+	}
+
+	void appendList(uint _count)
+	{
+		if (_count < 0x38)
+			m_out.push_back(_count | 0x80);
+		else
+			pushCount(_count, 0x80);
+	}
+
+	RLPStream& operator<<(uint _i) { append(_i); return *this; }
+	RLPStream& operator<<(u256 _i) { append(_i); return *this; }
+	RLPStream& operator<<(bigint _i) { append(_i); return *this; }
+	RLPStream& operator<<(char const* _s) { append(std::string(_s)); return *this; }
+	RLPStream& operator<<(std::string const& _s) { append(_s); return *this; }
+	RLPStream& operator<<(RLPList _l) { appendList(_l.count); return *this; }
+
+	bytes const& out() const { return m_out; }
+	std::string str() const { return std::string((char const*)m_out.data(), (char const*)(m_out.data() + m_out.size())); }
+
+private:
+	void appendNumeric(uint _i)
+	{
+		if (_i < 0x18)
+			m_out.push_back(_i);
+		else
+		{
+			auto br = bytesRequired(_i);
+			m_out.push_back(br + 0x17);	// max 8 bytes.
+			pushInt(_i, br);
+		}
+	}
+
+	void appendNumeric(u256 _i)
+	{
+		if (_i < 0x18)
+			m_out.push_back((byte)_i);
+		else
+		{
+			auto br = bytesRequired(_i);
+			m_out.push_back(br + 0x17);	// max 8 bytes.
+			pushInt(_i, br);
+		}
+	}
+
+	void appendNumeric(bigint _i)
+	{
+		if (_i < 0x18)
+			m_out.push_back((byte)_i);
+		else
+		{
+			uint br = bytesRequired(_i);
+			if (br <= 32)
+				m_out.push_back(bytesRequired(_i) + 0x17);	// max 32 bytes.
+			else
+			{
+				auto brbr = bytesRequired(br);
+				m_out.push_back(0x37 + brbr);
+				pushInt(br, brbr);
+			}
+			pushInt(_i, br);
+		}
+	}
+
+	template <class _T> void pushInt(_T _i, uint _br)
+	{
+		m_out.resize(m_out.size() + _br);
+		byte* b = &m_out.back();
+		for (; _i; _i >>= 8)
+			*(b--) = (byte)_i;
+	}
+
+	void pushCount(uint _count, byte _base)
+	{
+		auto br = bytesRequired(_count);
+		m_out.push_back(br + 0x37 + _base);	// max 8 bytes.
+		pushInt(_count, br);
+	}
+
+	template <class _T> static uint bytesRequired(_T _i)
+	{
+		_i >>= 8;
+		uint i = 1;
+		for (; _i != 0; ++i, _i >>= 8) {}
+		return i;
+	}
+
+	bytes m_out;
+};
+
+}
+
+inline std::string escaped(std::string const& _s)
+{
+	std::string ret;
+	ret.reserve(_s.size());
+	ret.push_back('"');
+	for (auto i: _s)
+		if (i == '"')
+			ret += "\\\"";
+		else if (i == '\\')
+			ret += "\\\\";
+		else if (i < ' ' || i > 127)
+		{
+			ret += "\\x";
+			ret.push_back("0123456789abcdef"[i / 16]);
+			ret.push_back("0123456789abcdef"[i % 16]);
+		}
+		else
+			ret.push_back(i);
+	ret.push_back('"');
+	return ret;
 }
 
 inline std::ostream& operator<<(std::ostream& _out, eth::RLP _d)
@@ -163,9 +322,9 @@ inline std::ostream& operator<<(std::ostream& _out, eth::RLP _d)
 	if (_d.isNull())
 		_out << "null";
 	else if (_d.isInt())
-		_out << _d.toBigInt();
+		_out << std::showbase << std::hex << std::nouppercase << _d.toBigInt();
 	else if (_d.isString())
-		_out << "\"" << _d.toString() << "\"";
+		_out << escaped(_d.toString());
 	else if (_d.isList())
 	{
 		_out << "[";
