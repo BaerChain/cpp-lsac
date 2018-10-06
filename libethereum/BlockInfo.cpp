1c617b3fd5d81c154f3c3b89390f52d6ab32296b
ssrbaer <ssrbaer@baerchain.net>
2018-10-05 19:50:36 -0600

    More flexible and stricter RLP int handling.

--- libethereum/BlockInfo.cpp
+++ libethereum/BlockInfo.cpp
@@ -1,9 +1,33 @@
+/*
+	This file is part of cpp-ethereum.
+
+	cpp-ethereum is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Foobar is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/** @file BlockInfo.cpp
+ * @author Gav Wood <i@gavwood.com>
+ * @date 2014
+ */
+
+#include "Common.h"
+#include "sha256.h"
+#include "Exceptions.h"
 #include "RLP.h"
 #include "BlockInfo.h"
 using namespace std;
 using namespace eth;
 
-void populateAndVerify(bytesConstRef _block, u256 _number)
+void BlockInfo::populateAndVerify(bytesConstRef _block, u256 _number)
 {
 	number = _number;
 
@@ -12,13 +36,13 @@ void populateAndVerify(bytesConstRef _block, u256 _number)
 	{
 		RLP header = root[0];
 		hash = eth::sha256(_block);
-		parentHash = header[0].toFatInt();
-		sha256Uncles = header[1].toFatInt();
-		coinbaseAddress = header[2].toFatInt();
-		sha256Transactions = header[3].toFatInt();
-		difficulty = header[4].toFatInt();
-		timestamp = header[5].toFatInt();
-		nonce = header[6].toFatInt();
+		parentHash = header[0].toInt<u256>();
+		sha256Uncles = header[1].toInt<u256>();
+		coinbaseAddress = header[2].toInt<u160>();
+		sha256Transactions = header[3].toInt<u256>();
+		difficulty = header[4].toInt<uint>();
+		timestamp = header[5].toInt<u256>();
+		nonce = header[6].toInt<u256>();
 	}
 	catch (RLP::BadCast)
 	{
