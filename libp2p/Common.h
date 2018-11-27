1dcd1026f5f6941a2570a503c522ea1df8603e3e
enixbaer <enixbaer@baerchain.net>
2018-11-26 19:44:55 -0600

    safe transaction injection

--- libp2p/Common.h
+++ libp2p/Common.h
@@ -16,10 +16,9 @@
 */
 /** @file Common.h
  * @author Gav Wood <i@gavwood.com>
- * @author Alex Leverington <nessence@gmail.com>
  * @date 2014
  *
- * Miscellanea required for the Host/Session/NodeTable classes.
+ * Miscellanea required for the Host/Session classes.
  */
 
 #pragma once
@@ -30,9 +29,9 @@
 #include <boost/asio.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <chrono>
-#include <libdevcrypto/Common.h>
+#include <libdevcore/Common.h>
 #include <libdevcore/Log.h>
-#include <libdevcore/Exceptions.h>
+#include <libdevcore/FixedHash.h>
 namespace ba = boost::asio;
 namespace bi = boost::asio::ip;
 
@@ -54,8 +53,6 @@ class UPnP;
 class Capability;
 class Host;
 class Session;
-
-struct NetworkStartRequired: virtual dev::Exception {};
 
 struct NetWarn: public LogChannel { static const char* name() { return "!N!"; } static const int verbosity = 0; };
 struct NetNote: public LogChannel { static const char* name() { return "*N*"; } static const int verbosity = 1; };
@@ -119,12 +116,7 @@ using CapDesc = std::pair<std::string, u256>;
 using CapDescSet = std::set<CapDesc>;
 using CapDescs = std::vector<CapDesc>;
 
-/*
- * Used by Host to pass negotiated information about a connection to a
- * new Peer Session; PeerSessionInfo is then maintained by Session and can
- * be queried for point-in-time status information via Host.
- */
-struct PeerSessionInfo
+struct PeerInfo
 {
 	NodeId id;
 	std::string clientVersion;
@@ -136,44 +128,7 @@ struct PeerSessionInfo
 	std::map<std::string, std::string> notes;
 };
 
-using PeerSessionInfos = std::vector<PeerSessionInfo>;
-
-/**
- * @brief IPv4,UDP/TCP endpoints.
- */
-struct NodeIPEndpoint
-{
-	NodeIPEndpoint(): udp(bi::udp::endpoint()), tcp(bi::tcp::endpoint()) {}
-	NodeIPEndpoint(bi::udp::endpoint _udp): udp(_udp) {}
-	NodeIPEndpoint(bi::tcp::endpoint _tcp): tcp(_tcp) {}
-	NodeIPEndpoint(bi::udp::endpoint _udp, bi::tcp::endpoint _tcp): udp(_udp), tcp(_tcp) {}
-
-	bi::udp::endpoint udp;
-	bi::tcp::endpoint tcp;
-	
-	operator bool() const { return !udp.address().is_unspecified() || !tcp.address().is_unspecified(); }
-};
-
-struct Node
-{
-	Node(): endpoint(NodeIPEndpoint()) {};
-	Node(Public _pubk, NodeIPEndpoint _ip, bool _required = false): id(_pubk), endpoint(_ip), required(_required) {}
-	Node(Public _pubk, bi::udp::endpoint _udp, bool _required = false): Node(_pubk, NodeIPEndpoint(_udp), _required) {}
-	
-	virtual NodeId const& address() const { return id; }
-	virtual Public const& publicKey() const { return id; }
-	
-	NodeId id;
-	
-	/// Endpoints by which we expect to reach node.
-	NodeIPEndpoint endpoint;
-	
-	/// If true, node will not be removed from Node list.
-	// TODO: p2p implement
-	bool required = false;
-	
-	virtual operator bool() const { return (bool)id; }
-};
+using PeerInfos = std::vector<PeerInfo>;
 
 }
 }
