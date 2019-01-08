1a51796e39001dee35869552a36fb604152a8806
gaerbaer <gaerbaer@baerchain.net>
2019-01-07 19:57:31 -0600

    Added missing source locations for new memory management code.

--- libweb3jsonrpc/AccountHolder.cpp
+++ libweb3jsonrpc/AccountHolder.cpp
@@ -13,114 +13,66 @@
 
 	You should have received a copy of the GNU General Public License
 	along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
-*/
-/** @file AccountHolder.cpp
- * @authors:
- *   Christian R <c@ethdev.com>
- *   Lefteris Karapetsas <lefteris@ethdev.com>
+ */
+/**
+ * @author Christian R <c@ethdev.com>
  * @date 2015
+ * Unit tests for the account holder used by the WebThreeStubServer.
  */
 
-#include "AccountHolder.h"
-#include <random>
-#include <ctime>
-#include <libdevcore/Guards.h>
-#include <libethereum/Client.h>
-#include <libethcore/KeyManager.h>
-
-
+#include <boost/test/unit_test.hpp>
+#include <libweb3jsonrpc/AccountHolder.h>
 using namespace std;
 using namespace dev;
-using namespace dev::eth;
+using namespace eth;
 
-vector<TransactionSkeleton> g_emptyQueue;
-static std::mt19937 g_randomNumberGenerator(time(0));
-static Mutex x_rngMutex;
-
-vector<Address> AccountHolder::allAccounts() const
+namespace dev
 {
-	vector<Address> accounts;
-	accounts += realAccounts();
-	for (auto const& pair: m_proxyAccounts)
-		if (!isRealAccount(pair.first))
-			accounts.push_back(pair.first);
-	return accounts;
+namespace test
+{
+
+BOOST_AUTO_TEST_SUITE(AccountHolderTest)
+
+BOOST_AUTO_TEST_CASE(ProxyAccountUseCase)
+{
+	FixedAccountHolder h = FixedAccountHolder(function<Interface*()>(), vector<KeyPair>());
+
+	BOOST_CHECK(h.allAccounts().empty());
+	BOOST_CHECK(h.realAccounts().empty());
+	Address addr("abababababababababababababababababababab");
+	Address addr2("abababababababababababababababababababab");
+	int id = h.addProxyAccount(addr);
+	BOOST_CHECK(h.queuedTransactions(id).empty());
+	// register it again
+	int secondID = h.addProxyAccount(addr);
+	BOOST_CHECK(h.queuedTransactions(secondID).empty());
+
+	eth::TransactionSkeleton t1;
+	eth::TransactionSkeleton t2;
+	t1.from = addr;
+	t1.data = fromHex("12345678");
+	t2.from = addr;
+	t2.data = fromHex("abcdef");
+	BOOST_CHECK(h.queuedTransactions(id).empty());
+	h.queueTransaction(t1);
+	BOOST_CHECK_EQUAL(1, h.queuedTransactions(id).size());
+	h.queueTransaction(t2);
+	BOOST_REQUIRE_EQUAL(2, h.queuedTransactions(id).size());
+
+	// second proxy should not see transactions
+	BOOST_CHECK(h.queuedTransactions(secondID).empty());
+
+	BOOST_CHECK(h.queuedTransactions(id)[0].data == t1.data);
+	BOOST_CHECK(h.queuedTransactions(id)[1].data == t2.data);
+
+	h.clearQueue(id);
+	BOOST_CHECK(h.queuedTransactions(id).empty());
+	// removing fails because it never existed
+	BOOST_CHECK(!h.removeProxyAccount(secondID));
+	BOOST_CHECK(h.removeProxyAccount(id));
 }
 
-Address const& AccountHolder::defaultTransactAccount() const
-{
-	auto accounts = realAccounts();
-	if (accounts.empty())
-		return ZeroAddress;
-	Address const* bestMatch = &*accounts.begin();
-	for (auto const& account: accounts)
-		if (m_client()->balanceAt(account) > m_client()->balanceAt(*bestMatch))
-			bestMatch = &account;
-	return *bestMatch;
+BOOST_AUTO_TEST_SUITE_END()
+
 }
-
-int AccountHolder::addProxyAccount(const Address& _account)
-{
-	Guard g(x_rngMutex);
-	int id = std::uniform_int_distribution<int>(1)(g_randomNumberGenerator);
-	id = int(u256(FixedHash<32>(sha3(bytesConstRef((byte*)(&id), sizeof(int) / sizeof(byte))))));
-	if (isProxyAccount(_account) || id == 0 || m_transactionQueues.count(id))
-		return 0;
-	m_proxyAccounts.insert(make_pair(_account, id));
-	m_transactionQueues[id].first = _account;
-	return id;
 }
-
-bool AccountHolder::removeProxyAccount(unsigned _id)
-{
-	if (!m_transactionQueues.count(_id))
-		return false;
-	m_proxyAccounts.erase(m_transactionQueues[_id].first);
-	m_transactionQueues.erase(_id);
-	return true;
-}
-
-void AccountHolder::queueTransaction(TransactionSkeleton const& _transaction)
-{
-	if (!m_proxyAccounts.count(_transaction.from))
-		return;
-	int id = m_proxyAccounts[_transaction.from];
-	m_transactionQueues[id].second.push_back(_transaction);
-}
-
-vector<TransactionSkeleton> const& AccountHolder::queuedTransactions(int _id) const
-{
-	if (!m_transactionQueues.count(_id))
-		return g_emptyQueue;
-	return m_transactionQueues.at(_id).second;
-}
-
-void AccountHolder::clearQueue(int _id)
-{
-	if (m_transactionQueues.count(_id))
-		m_transactionQueues.at(_id).second.clear();
-}
-
-AddressHash SimpleAccountHolder::realAccounts() const
-{
-	return m_keyManager.accounts();
-}
-
-void SimpleAccountHolder::authenticate(dev::eth::TransactionSkeleton const& _t)
-{
-	if (isRealAccount(_t.from))
-		m_client()->submitTransaction(m_keyManager.secret(_t.from, [&](){ return m_getPassword(_t.from); }), _t);
-	else if (isProxyAccount(_t.from))
-		queueTransaction(_t);
-}
-
-void FixedAccountHolder::authenticate(dev::eth::TransactionSkeleton const& _t)
-{
-	if (isRealAccount(_t.from))
-		m_client()->submitTransaction(m_accounts[_t.from], _t);
-	else if (isProxyAccount(_t.from))
-		queueTransaction(_t);
-}
-
-
-
