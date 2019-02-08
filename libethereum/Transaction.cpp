04ce54342f886408cf417f95cac635e3328a7391
thqbaer <thqbaer@baerchain.net>
2019-02-07 19:50:04 -0600

    boost issue

--- libethereum/Transaction.cpp
+++ libethereum/Transaction.cpp
@@ -15,95 +15,155 @@
 	along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
 */
 /** @file Transaction.cpp
- * @author Gav Wood <i@gavwood.com>
- * @date 2014
+ * @author Dmitrii Khokhlov <winsvega@mail.ru>
+ * @date 2015
+ * Transaaction test functions.
  */
 
-#include <libdevcore/vector_ref.h>
-#include <libdevcore/Log.h>
-#include <libdevcore/CommonIO.h>
-#include <libdevcrypto/Common.h>
+#include "test/TestHelper.h"
 #include <libethcore/Exceptions.h>
 #include <libevm/VMFace.h>
-#include "Interface.h"
-#include "Transaction.h"
-using namespace std;
+#include <libethcore/Common.h>
+
 using namespace dev;
-using namespace dev::eth;
+using namespace eth;
 
-#define ETH_ADDRESS_DEBUG 0
+BOOST_AUTO_TEST_SUITE(libethereum)
 
-std::ostream& dev::eth::operator<<(std::ostream& _out, ExecutionResult const& _er)
+BOOST_AUTO_TEST_CASE(TransactionGasRequired)
 {
-	_out << "{" << _er.gasUsed << ", " << _er.newAddress << ", " << toHex(_er.output) << "}";
-	return _out;
+	Transaction tr(fromHex("0xf86d800182521c94095e7baea6a6c7c4c2dfeb977efac326af552d870a8e0358ac39584bc98a7c979f984b031ba048b55bfa915ac795c431978d8a6a992b628d557da5ff759b307d495a36649353a0efffd310ac743f371de3b9f7f9cb56c0b28ad43601b4ab949f53faa07bd2c804"), CheckTransaction::None);
+	BOOST_CHECK_MESSAGE(tr.gasRequired() == 21952, "Transaction::GasRequired() has changed!");
 }
 
-TransactionException dev::eth::toTransactionException(Exception const& _e)
+BOOST_AUTO_TEST_CASE(TransactionConstructor)
 {
-	// Basic Transaction exceptions
-	if (!!dynamic_cast<RLPException const*>(&_e))
-		return TransactionException::BadRLP;
-	if (!!dynamic_cast<OutOfGasIntrinsic const*>(&_e))
-		return TransactionException::OutOfGasIntrinsic;
-	if (!!dynamic_cast<InvalidSignature const*>(&_e))
-		return TransactionException::InvalidSignature;
-	// Executive exceptions
-	if (!!dynamic_cast<OutOfGasBase const*>(&_e))
-		return TransactionException::OutOfGasBase;
-	if (!!dynamic_cast<InvalidNonce const*>(&_e))
-		return TransactionException::InvalidNonce;
-	if (!!dynamic_cast<NotEnoughCash const*>(&_e))
-		return TransactionException::NotEnoughCash;
-	if (!!dynamic_cast<BlockGasLimitReached const*>(&_e))
-		return TransactionException::BlockGasLimitReached;
-	// VM execution exceptions
-	if (!!dynamic_cast<BadInstruction const*>(&_e))
-		return TransactionException::BadInstruction;
-	if (!!dynamic_cast<BadJumpDestination const*>(&_e))
-		return TransactionException::BadJumpDestination;
-	if (!!dynamic_cast<OutOfGas const*>(&_e))
-		return TransactionException::OutOfGas;
-	if (!!dynamic_cast<OutOfStack const*>(&_e))
-		return TransactionException::OutOfStack;
-	if (!!dynamic_cast<StackUnderflow const*>(&_e))
-		return TransactionException::StackUnderflow;
-	return TransactionException::Unknown;
-}
-
-std::ostream& dev::eth::operator<<(std::ostream& _out, TransactionException const& _er)
-{
-	switch (_er)
+	bool wasException = false;
+	try
 	{
-		case TransactionException::None: _out << "None"; break;
-		case TransactionException::BadRLP: _out << "BadRLP"; break;
-		case TransactionException::InvalidFormat: _out << "InvalidFormat"; break;
-		case TransactionException::OutOfGasIntrinsic: _out << "OutOfGasIntrinsic"; break;
-		case TransactionException::InvalidSignature: _out << "InvalidSignature"; break;
-		case TransactionException::InvalidNonce: _out << "InvalidNonce"; break;
-		case TransactionException::NotEnoughCash: _out << "NotEnoughCash"; break;
-		case TransactionException::OutOfGasBase: _out << "OutOfGasBase"; break;
-		case TransactionException::BlockGasLimitReached: _out << "BlockGasLimitReached"; break;
-		case TransactionException::BadInstruction: _out << "BadInstruction"; break;
-		case TransactionException::BadJumpDestination: _out << "BadJumpDestination"; break;
-		case TransactionException::OutOfGas: _out << "OutOfGas"; break;
-		case TransactionException::OutOfStack: _out << "OutOfStack"; break;
-		case TransactionException::StackUnderflow: _out << "StackUnderflow"; break;
-		default: _out << "Unknown"; break;
+		Transaction(fromHex("0xf86d800182521c94095e7baea6a6c7c4c2dfeb977efac326af552d870a8e0358ac39584bc98a7c979f984b031ba048b55bfa915ac795c431978d8a6a992b628d557da5ff759b307d495a36649353a0efffd310ac743f371de3b9f7f9cb56c0b28ad43601b4ab949f53faa07bd2c804"), CheckTransaction::Everything);
 	}
-	return _out;
+	catch (OutOfGasIntrinsic)
+	{
+		wasException = true;
+	}
+	catch (Exception)
+	{
+		BOOST_ERROR("Exception thrown but expected OutOfGasIntrinsic instead");
+	}
+
+	BOOST_CHECK_MESSAGE(wasException, "Expected OutOfGasIntrinsic exception to be thrown at TransactionConstructor test");
 }
 
-Transaction::Transaction(bytesConstRef _rlpData, CheckTransaction _checkSig):
-	TransactionBase(_rlpData, _checkSig)
+BOOST_AUTO_TEST_CASE(ExecutionResultOutput)
 {
-	if (_checkSig >= CheckTransaction::Cheap && !checkPayment())
-		BOOST_THROW_EXCEPTION(OutOfGasIntrinsic() << RequirementError(gasRequired(), (bigint)gas()));
+	std::stringstream buffer;
+	ExecutionResult exRes;
+
+	exRes.gasUsed = u256("12345");
+	exRes.newAddress = Address("a94f5374fce5edbc8e2a8697c15331677e6ebf0b");
+	exRes.output = fromHex("001122334455");
+
+	buffer << exRes;
+	BOOST_CHECK_MESSAGE(buffer.str() == "{12345, a94f5374fce5edbc8e2a8697c15331677e6ebf0b, 001122334455}", "Error ExecutionResultOutput");
 }
 
-bigint Transaction::gasRequired() const
+BOOST_AUTO_TEST_CASE(transactionExceptionOutput)
 {
-	if (!m_gasRequired)
-		m_gasRequired = Transaction::gasRequired(m_data);
-	return m_gasRequired;
+	std::stringstream buffer;
+	buffer << TransactionException::BadInstruction;
+	BOOST_CHECK_MESSAGE(buffer.str() == "BadInstruction", "Error output TransactionException::BadInstruction");
+	buffer.str(std::string());
+
+	buffer << TransactionException::None;
+	BOOST_CHECK_MESSAGE(buffer.str() == "None", "Error output TransactionException::None");
+	buffer.str(std::string());
+
+	buffer << TransactionException::BadRLP;
+	BOOST_CHECK_MESSAGE(buffer.str() == "BadRLP", "Error output TransactionException::BadRLP");
+	buffer.str(std::string());
+
+	buffer << TransactionException::InvalidFormat;
+	BOOST_CHECK_MESSAGE(buffer.str() == "InvalidFormat", "Error output TransactionException::InvalidFormat");
+	buffer.str(std::string());
+
+	buffer << TransactionException::OutOfGasIntrinsic;
+	BOOST_CHECK_MESSAGE(buffer.str() == "OutOfGasIntrinsic", "Error output TransactionException::OutOfGasIntrinsic");
+	buffer.str(std::string());
+
+	buffer << TransactionException::InvalidSignature;
+	BOOST_CHECK_MESSAGE(buffer.str() == "InvalidSignature", "Error output TransactionException::InvalidSignature");
+	buffer.str(std::string());
+
+	buffer << TransactionException::InvalidNonce;
+	BOOST_CHECK_MESSAGE(buffer.str() == "InvalidNonce", "Error output TransactionException::InvalidNonce");
+	buffer.str(std::string());
+
+	buffer << TransactionException::NotEnoughCash;
+	BOOST_CHECK_MESSAGE(buffer.str() == "NotEnoughCash", "Error output TransactionException::NotEnoughCash");
+	buffer.str(std::string());
+
+	buffer << TransactionException::OutOfGasBase;
+	BOOST_CHECK_MESSAGE(buffer.str() == "OutOfGasBase", "Error output TransactionException::OutOfGasBase");
+	buffer.str(std::string());
+
+	buffer << TransactionException::BlockGasLimitReached;
+	BOOST_CHECK_MESSAGE(buffer.str() == "BlockGasLimitReached", "Error output TransactionException::BlockGasLimitReached");
+	buffer.str(std::string());
+
+	buffer << TransactionException::BadInstruction;
+	BOOST_CHECK_MESSAGE(buffer.str() == "BadInstruction", "Error output TransactionException::BadInstruction");
+	buffer.str(std::string());
+
+	buffer << TransactionException::BadJumpDestination;
+	BOOST_CHECK_MESSAGE(buffer.str() == "BadJumpDestination", "Error output TransactionException::BadJumpDestination");
+	buffer.str(std::string());
+
+	buffer << TransactionException::OutOfGas;
+	BOOST_CHECK_MESSAGE(buffer.str() == "OutOfGas", "Error output TransactionException::OutOfGas");
+	buffer.str(std::string());
+
+	buffer << TransactionException::OutOfStack;
+	BOOST_CHECK_MESSAGE(buffer.str() == "OutOfStack", "Error output TransactionException::OutOfStack");
+	buffer.str(std::string());
+
+	buffer << TransactionException::StackUnderflow;
+	BOOST_CHECK_MESSAGE(buffer.str() == "StackUnderflow", "Error output TransactionException::StackUnderflow");
+	buffer.str(std::string());
+
+	buffer << TransactionException(-1);
+	BOOST_CHECK_MESSAGE(buffer.str() == "Unknown", "Error output TransactionException::StackUnderflow");
+	buffer.str(std::string());
 }
+
+BOOST_AUTO_TEST_CASE(toTransactionExceptionConvert)
+{
+	RLPException rlpEx("exception");//toTransactionException(*(dynamic_cast<Exception*>
+	BOOST_CHECK_MESSAGE(toTransactionException(rlpEx) == TransactionException::BadRLP, "RLPException !=> TransactionException");
+	OutOfGasIntrinsic oogEx;
+	BOOST_CHECK_MESSAGE(toTransactionException(oogEx) == TransactionException::OutOfGasIntrinsic, "OutOfGasIntrinsic !=> TransactionException");
+	InvalidSignature sigEx;
+	BOOST_CHECK_MESSAGE(toTransactionException(sigEx) == TransactionException::InvalidSignature, "InvalidSignature !=> TransactionException");
+	OutOfGasBase oogbEx;
+	BOOST_CHECK_MESSAGE(toTransactionException(oogbEx) == TransactionException::OutOfGasBase, "OutOfGasBase !=> TransactionException");
+	InvalidNonce nonceEx;
+	BOOST_CHECK_MESSAGE(toTransactionException(nonceEx) == TransactionException::InvalidNonce, "InvalidNonce !=> TransactionException");
+	NotEnoughCash cashEx;
+	BOOST_CHECK_MESSAGE(toTransactionException(cashEx) == TransactionException::NotEnoughCash, "NotEnoughCash !=> TransactionException");
+	BlockGasLimitReached blGasEx;
+	BOOST_CHECK_MESSAGE(toTransactionException(blGasEx) == TransactionException::BlockGasLimitReached, "BlockGasLimitReached !=> TransactionException");
+	BadInstruction badInsEx;
+	BOOST_CHECK_MESSAGE(toTransactionException(badInsEx) == TransactionException::BadInstruction, "BadInstruction !=> TransactionException");
+	BadJumpDestination badJumpEx;
+	BOOST_CHECK_MESSAGE(toTransactionException(badJumpEx) == TransactionException::BadJumpDestination, "BadJumpDestination !=> TransactionException");
+	OutOfGas oogEx2;
+	BOOST_CHECK_MESSAGE(toTransactionException(oogEx2) == TransactionException::OutOfGas, "OutOfGas !=> TransactionException");
+	OutOfStack oosEx;
+	BOOST_CHECK_MESSAGE(toTransactionException(oosEx) == TransactionException::OutOfStack, "OutOfStack !=> TransactionException");
+	StackUnderflow stackEx;
+	BOOST_CHECK_MESSAGE(toTransactionException(stackEx) == TransactionException::StackUnderflow, "StackUnderflow !=> TransactionException");
+	Exception notEx;
+	BOOST_CHECK_MESSAGE(toTransactionException(notEx) == TransactionException::Unknown, "Unexpected should be TransactionException::Unknown");
+}
+
+BOOST_AUTO_TEST_SUITE_END()
--- libjsengine/JSV8Engine.cpp
+++ libjsengine/JSV8Engine.cpp
@@ -1,203 +1,71 @@
-/*
-	This file is part of cpp-ethereum.
+//
+// Created by Marek Kotewicz on 27/04/15.
+//
 
-	cpp-ethereum is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	cpp-ethereum is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
-*/
-/** @file JSV8Engine.cpp
- * @author Marek Kotewicz <marek@ethdev.com>
- * @date 2015
- * Ethereum client.
- */
-
-#include <memory>
-#include "JSV8Engine.h"
-#include "JSV8Printer.h"
-#include "libjsengine/JSEngineResources.hpp"
-#include "BuildInfo.h"
-
-#define TO_STRING_HELPER(s) #s
-#define TO_STRING(s) TO_STRING_HELPER(s)
+#include <boost/test/unit_test.hpp>
+#include <libjsengine/JSV8Engine.h>
+#include <libjsengine/JSV8Printer.h>
 
 using namespace std;
 using namespace dev;
 using namespace dev::eth;
-using namespace v8;
 
-namespace dev
-{
-namespace eth
-{
+BOOST_AUTO_TEST_SUITE(jsv8engine)
 
-static char const* toCString(String::Utf8Value const& _value)
+BOOST_AUTO_TEST_CASE(evalInteger)
 {
-	if (*_value)
-		return *_value;
-	throw JSPrintException();
+	JSV8Engine engine;
+	JSV8Printer printer(engine);
+	auto value = engine.eval("1 + 1");
+	string result = printer.print(value).cstr();
+	BOOST_CHECK_EQUAL(result, "2");
 }
 
-// from:        https://github.com/v8/v8-git-mirror/blob/master/samples/shell.cc
-// v3.15 from:  https://chromium.googlesource.com/v8/v8.git/+/3.14.5.9/samples/shell.cc
-void reportException(TryCatch* _tryCatch)
+BOOST_AUTO_TEST_CASE(evalString)
 {
-	HandleScope handle_scope;
-	String::Utf8Value exception(_tryCatch->Exception());
-	char const* exceptionString = toCString(exception);
-	Handle<Message> message = _tryCatch->Message();
-
-	// V8 didn't provide any extra information about this error; just
-	// print the exception.
-	if (message.IsEmpty())
-		printf("%s\n", exceptionString);
-	else
-	{
-		// Print (filename):(line number): (message).
-		String::Utf8Value filename(message->GetScriptResourceName());
-		char const* filenameString = toCString(filename);
-		int linenum = message->GetLineNumber();
-		printf("%s:%i: %s\n", filenameString, linenum, exceptionString);
-
-		// Print line of source code.
-		String::Utf8Value sourceline(message->GetSourceLine());
-		char const* sourcelineString = toCString(sourceline);
-		printf("%s\n", sourcelineString);
-
-		// Print wavy underline (GetUnderline is deprecated).
-		int start = message->GetStartColumn();
-		for (int i = 0; i < start; i++)
-			printf(" ");
-
-		int end = message->GetEndColumn();
-
-		for (int i = start; i < end; i++)
-			printf("^");
-
-		printf("\n");
-
-		String::Utf8Value stackTrace(_tryCatch->StackTrace());
-		if (stackTrace.length() > 0)
-		{
-			char const* stackTraceString = toCString(stackTrace);
-			printf("%s\n", stackTraceString);
-		}
-	}
+	JSV8Engine engine;
+	JSV8Printer printer(engine);
+	auto value = engine.eval("'hello ' + 'world'");
+	string result = printer.print(value).cstr();
+	BOOST_CHECK_EQUAL(result, "hello world");
 }
 
-Handle<Value> ConsoleLog(Arguments const& _args)
+BOOST_AUTO_TEST_CASE(evalEmpty)
 {
-	Local<External> wrap = Local<External>::Cast(_args.Data());
-	auto engine = reinterpret_cast<JSV8Engine const*>(wrap->Value());
-	JSV8Printer printer(*engine);
-	for (int i = 0; i < _args.Length(); ++i)
-		printf("%s\n", printer.prettyPrint(_args[i]).cstr());
-	return Undefined();
+	JSV8Engine engine;
+	JSV8Printer printer(engine);
+	auto value = engine.eval("");
+	string result = printer.print(value).cstr();
+	BOOST_CHECK_EQUAL(result, "undefined");
 }
 
-
-class JSV8Scope
+BOOST_AUTO_TEST_CASE(evalAssignment)
 {
-public:
-	JSV8Scope():
-		m_handleScope(),
-		m_context(Context::New(NULL, ObjectTemplate::New())),
-		m_contextScope(m_context)
-	{
-		m_context->Enter();
-	}
-
-	~JSV8Scope()
-	{
-		m_context->Exit();
-		m_context.Dispose();
-	}
-
-	Persistent <Context> const& context() const { return m_context; }
-
-private:
-	HandleScope m_handleScope;
-	Persistent <Context> m_context;
-	Context::Scope m_contextScope;
-};
-
-}
+	JSV8Engine engine;
+	JSV8Printer printer(engine);
+	auto value = engine.eval("x = 5");
+	string result = printer.print(value).cstr();
+	BOOST_CHECK_EQUAL(result, "5");
 }
 
-JSString JSV8Value::toString() const
+BOOST_AUTO_TEST_CASE(evalIncorrectExpression)
 {
-	if (m_value.IsEmpty())
-		return "";
-
-	else if (m_value->IsUndefined())
-		return "undefined";
-
-	String::Utf8Value str(m_value);
-	return toCString(str);
+	JSV8Engine engine;
+	JSV8Printer printer(engine);
+	auto value = engine.eval("[");
+	string result = printer.print(value).cstr();
+	BOOST_CHECK_EQUAL(result, "Error: Uncaught SyntaxError: Unexpected end of input");
 }
 
-JSV8Engine::JSV8Engine(): m_scope(new JSV8Scope())
+BOOST_AUTO_TEST_CASE(evalNull)
 {
-	JSEngineResources resources;
-	eval("env = typeof(env) === 'undefined' ? {} : env; env.os = '" TO_STRING(ETH_BUILD_PLATFORM) "'");
-	string common = resources.loadResourceAsString("common");
-	string web3 = resources.loadResourceAsString("web3");
-	string admin = resources.loadResourceAsString("admin");
-	eval(common.c_str());
-	eval(web3.c_str());
-	eval("web3 = require('web3');");
-	eval(admin.c_str());
-
-	auto consoleTemplate = ObjectTemplate::New();
-
-	Local<FunctionTemplate> function = FunctionTemplate::New(ConsoleLog, External::New(this));
-	consoleTemplate->Set(String::New("debug"), function);
-	consoleTemplate->Set(String::New("log"), function);
-	consoleTemplate->Set(String::New("error"), function);
-	context()->Global()->Set(String::New("console"), consoleTemplate->NewInstance());
+	JSV8Engine engine;
+	JSV8Printer printer(engine);
+	auto value = engine.eval("null");
+	string result = printer.print(value).cstr();
+	string prettyResult = printer.prettyPrint(value).cstr();
+	BOOST_CHECK_EQUAL(result, "null");
+	BOOST_CHECK_EQUAL(prettyResult.find("null") != std::string::npos, true);
 }
 
-JSV8Engine::~JSV8Engine()
-{
-	delete m_scope;
-}
-
-JSV8Value JSV8Engine::eval(char const* _cstr) const
-{
-	TryCatch tryCatch;
-	Local<String> source = String::New(_cstr);
-	Local<String> name(String::New("(shell)"));
-	ScriptOrigin origin(name);
-	Handle<Script> script = Script::Compile(source, &origin);
-
-	// Make sure to wrap the exception in a new handle because
-	// the handle returned from the TryCatch is destroyed
-	if (script.IsEmpty())
-	{
-		reportException(&tryCatch);
-		return Exception::Error(Local<String>::New(tryCatch.Message()->Get()));
-	}
-
-	auto result = script->Run();
-
-	if (result.IsEmpty())
-	{
-		reportException(&tryCatch);
-		return Exception::Error(Local<String>::New(tryCatch.Message()->Get()));
-	}
-
-	return result;
-}
-
-Handle<Context> const& JSV8Engine::context() const
-{
-	return m_scope->context();
-}
+BOOST_AUTO_TEST_SUITE_END()
