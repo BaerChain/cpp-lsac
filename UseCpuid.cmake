2ffd4bb7c1190990444196dffd597391d6c15964
miraclebaer <miraclebaer@baerchain.net>
2019-02-10 19:37:21 -0600

    consistency update

--- UseCpuid.cmake
+++
@@ -1,8 +0,0 @@
-function(eth_apply TARGET REQUIRED)	
-	if (CPUID_FOUND)
-		target_include_directories(${TARGET} SYSTEM PUBLIC ${CPUID_INCLUDE_DIRS})
-		target_link_libraries(${TARGET} ${CPUID_LIBRARIES})
-	elseif (NOT ${REQUIRED} STREQUAL "OPTIONAL")
-		message(FATAL_ERROR "CPUID library not found")
-	endif()
-endfunction()
