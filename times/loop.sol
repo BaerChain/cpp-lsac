e51b92293f969c6c3dff5203866c29f9d402e266
segabaer <segabaer@baerchain.net>
2019-02-25 19:46:19 -0600

    leveldb debug shlwapi dependency

--- times/loop.sol
+++
@@ -1,7 +0,0 @@
-contract loop {
-
-    function loop(){
-        for (int i = 0; i < 100000; ++1) {
-        }
-    }
-}
--- times/rng.sh
+++
@@ -1,5 +0,0 @@
-time ~/webthree-umbrella/build/libethereum/ethvm/ethvm --gas-limit 100000000 --gas 10000000 --vm interpreter rng.bin
-
-time ~/webthree-umbrella/build/libethereum/ethvm/ethvm --gas-limit 100000000 --gas 10000000 --vm interpreter output rng.bin
-
-time ~/webthree-umbrella/build/libethereum/ethvm/ethvm --gas-limit 100000000 --gas 10000000 --vm jit output rng.bin
