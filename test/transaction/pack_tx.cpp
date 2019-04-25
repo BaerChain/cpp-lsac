18d8755ba50afa12a059011c651e3ebf7a89165a
magicbaer <magicbaer@baerchain.net>
2019-04-25 16:32:51 +0800

    many thread  for test rlp

--- test/transaction/pack_tx.cpp
+++ test/transaction/pack_tx.cpp
@@ -12,9 +12,77 @@
 #include <boost/format.hpp>
 #include <libdevcrypto/base58.h>
 #include <libbrcdchain/Transaction.h>
+#include <iostream>
+#include <future>
+#include <thread>
+#include <vector>
 
 using namespace dev;
 using namespace dev::brc::transationTool;
+
+
+
+template <typename T, typename R>
+struct Task{
+    typedef  std::vector<T>     value_source;
+    typedef  std::vector<R>     value_result;
+
+    Task(uint32_t numbers = 4):m_thread(numbers){
+
+    }
+
+
+    template <typename CONVERT>
+    void go_task(const value_source &source, value_result &result, CONVERT conver_func){
+        std::vector<std::vector<T>>  split_source(m_thread);
+        std::vector<std::vector<R>>  split_result(m_thread);
+
+        auto one_size = source.size() / m_thread;
+        auto sp_size = source.size() % one_size;        //find surplus
+        std::vector<std::future<value_result>> vec_ff;
+
+
+        for(int i = 0; i < m_thread; i++){
+            if(i == m_thread - 1 && sp_size != 0){  //add surplus to last thread
+                split_source[i].assign(source.begin() + i * one_size, source.begin() + (i + 1) * one_size + sp_size);
+            }else{
+                split_source[i].assign(source.begin() + i * one_size, source.begin() + (i + 1) * one_size);
+            }
+            std::packaged_task<value_result(value_source)>  task([&](const value_source &d){
+                value_result ret;
+                for(auto itr : d){
+                    ret.push_back( conver_func(itr));
+
+                }
+                return ret;
+            });
+            vec_ff.push_back(task.get_future());
+            std::thread(std::move(task), split_source[i]).detach();
+        }
+
+
+        for(int i = 0; i < m_thread; i++){
+            vec_ff[i].wait();
+            split_result[i] = vec_ff[i].get();
+        }
+
+        for(auto i = 0 ; i < m_thread; i++){
+            result.insert(result.end(), split_result[i].begin(), split_result[i].end());
+        }
+
+
+    }
+
+
+
+private:
+    uint32_t  m_thread;
+};
+
+
+
+
+
 
 BOOST_AUTO_TEST_SUITE(test_pack_tx)
 
@@ -29,7 +97,7 @@ BOOST_AUTO_TEST_SUITE(test_pack_tx)
         bytes data = cancel_op.serialize();
 
 
-        for(auto i = 0 ; i < 1000; i++){
+        for(auto i = 0 ; i < 1001; i++){
             auto key_pair = KeyPair::create();
             auto sec = key_pair.secret();
             address_secret[key_pair.address()] = key_pair.secret();
@@ -50,15 +118,30 @@ BOOST_AUTO_TEST_SUITE(test_pack_tx)
             vec_trx.push_back(sign_t.rlp());
         }
 
+
         {
             Timer  t;
+            std::vector<dev::brc::Transaction> ret1;
             for(auto &itr : vec_trx){
                 dev::brc::Transaction t(itr, dev::brc::CheckTransaction::Everything);
+                ret1.push_back(t);
             }
 
             std::cout << t.elapsed() << std::endl;
-        }
+            for(auto count = 1; count < 10; count++){
+                t.restart();
+                Task<bytes, dev::brc::Transaction> task(count);
+                std::vector<dev::brc::Transaction> ret;
+                task.go_task(vec_trx, ret, [](const bytes &b){
+                    return dev::brc::Transaction(b, dev::brc::CheckTransaction::Everything);
+                });
 
+                std::cout << "t : " << count << "  " << t.elapsed() << std::endl;
+
+                BOOST_CHECK(ret1 == ret);
+            }
+
+        }
 
 
 
