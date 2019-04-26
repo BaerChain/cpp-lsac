1d2df24b2e2de6a2c93e96885e5897ef8c8eb81a
magicbaer <magicbaer@baerchain.net>
2019-04-26 16:46:53 +0800

    test

--- test/transaction/pack_tx.cpp
+++ test/transaction/pack_tx.cpp
@@ -16,7 +16,8 @@
 #include <future>
 #include <thread>
 #include <vector>
-
+#include <boost/thread.hpp>
+#include <boost/lockfree/queue.hpp>
 using namespace dev;
 using namespace dev::brc::transationTool;
 
@@ -77,8 +78,101 @@ private:
 };
 
 
+template <typename T, typename R>
+struct task_work {
+
+    typedef std::vector<T>      source_value;
+    typedef std::vector<R>      result_value;
+    typedef std::function<R(const T&)> CONVERT;
+    struct request_context {
+        source_value                *input = nullptr;
+        result_value                *output= nullptr;
+        CONVERT                     ff = nullptr;
+        boost::promise<void>        *ret = nullptr;
+
+        bool empty()
+        {
+            return input != nullptr && output != nullptr && ret != nullptr;
+        }
+
+    };
+
+    task_work(uint32_t threads) : m_thread(threads),m_queue(threads * 2), m_running(true),m_sleep_times(1000 * 1){
+        for(auto i = 0; i < threads; i++){
+            m_th.push_back(boost::thread([&](){
+                request_context *cxt = nullptr;
+                while(m_running){
+
+                    while (m_queue.pop(cxt)){
+                        if(cxt->empty()){
+                            for(auto itr = cxt->input->begin(); itr != cxt->input->end(); itr++){
+                                cxt->output->push_back( cxt->ff(*itr));
+                            }
+                            cxt->ret->set_value();
+                        }
+                        else{
+                            if(cxt->ret){
+                                cxt->ret->set_value();
+                            }
+                        }
+                    }
+                    boost::this_thread::sleep(boost::posix_time::microseconds(m_sleep_times));
+                }
+            }));
+            m_th[i].detach();
+        }
+    }
 
 
+    void add_task(const source_value &source, result_value &result, CONVERT f){
+        std::vector<std::vector<T>>  split_source(m_thread);
+        std::vector<std::vector<R>>  split_result(m_thread);
+
+        auto one_size = source.size() / m_thread;
+        auto sp_size = source.size() % one_size;        //find surplus
+
+        std::vector< boost::promise<void> >     proms(m_thread);
+        std::vector< request_context>           contexts(m_thread);
+        for(int i = 0; i < m_thread; i++){
+            if(i == m_thread - 1 && sp_size != 0){  //add surplus to last thread
+
+                split_source[i].assign(source.begin() + i * one_size, source.begin() + (i + 1) * one_size + sp_size);
+
+                contexts[i].ff = f;
+                contexts[i].input = &split_source[i];
+                contexts[i].output = &split_result[i];
+                contexts[i].ret = &proms[i];
+                m_queue.push(&contexts[i]);
+
+
+            }else{
+                split_source[i].assign(source.begin() + i * one_size, source.begin() + (i + 1) * one_size);
+
+
+                contexts[i].ff = f;
+                contexts[i].input = &split_source[i];
+                contexts[i].output = &split_result[i];
+                contexts[i].ret = &proms[i];
+                m_queue.push(&contexts[i]);
+            }
+
+        }
+
+        for(auto &itr : proms){
+            itr.get_future().get();
+        }
+        for(auto i = 0 ; i < m_thread; i++){
+            result.insert(result.end(), split_result[i].begin(), split_result[i].end());
+        }
+    }
+
+private:
+    uint32_t                                    m_thread;
+    boost::lockfree::queue<request_context *>   m_queue;
+    std::vector<boost::thread>                  m_th;
+    uint32_t                                    m_sleep_times;      //micrseconds
+    bool                                        m_running;
+};
 
 
 BOOST_AUTO_TEST_SUITE(test_pack_tx)
@@ -94,7 +188,7 @@ BOOST_AUTO_TEST_SUITE(test_pack_tx)
         bytes data = cancel_op.serialize();
 
 
-        for(auto i = 0 ; i < 10001; i++){
+        for(auto i = 0 ; i < 1001; i++){
             auto key_pair = KeyPair::create();
             auto sec = key_pair.secret();
             address_secret[key_pair.address()] = key_pair.secret();
@@ -116,28 +210,51 @@ BOOST_AUTO_TEST_SUITE(test_pack_tx)
         }
 
 
-        {
-            Timer  t;
-            std::vector<dev::brc::Transaction> ret1;
-            for(auto &itr : vec_trx){
-                dev::brc::Transaction t(itr, dev::brc::CheckTransaction::Everything);
-                ret1.push_back(t);
-            }
+        Timer  t;
+        std::vector<dev::brc::Transaction> ret1;
+        for(auto &itr : vec_trx){
+            dev::brc::Transaction t(itr, dev::brc::CheckTransaction::Everything);
+            ret1.push_back(t);
+        }
 
-            std::cout << t.elapsed() << std::endl;
-            for(auto count = 1; count < 12; count++){
+        std::cout << t.elapsed() << std::endl;
+        {
+            Timer all;
+            Timer  t;
+
+            for(auto count = 1; count < 100; count++){
                 t.restart();
-                Task<bytes, dev::brc::Transaction> task(count);
+                Task<bytes, dev::brc::Transaction> task(6);
                 std::vector<dev::brc::Transaction> ret;
                 task.go_task(vec_trx, ret, [](const bytes &b){
                     return dev::brc::Transaction(b, dev::brc::CheckTransaction::Everything);
                 });
 
-                std::cout << "t : " << count << "  " << t.elapsed() << std::endl;
+//                std::cout << "t : " << count << "  " << t.elapsed() << std::endl;
 
-                BOOST_CHECK(ret1 == ret);
+//                BOOST_CHECK(ret1 == ret);
             }
 
+            std::cout << "all : " << all.elapsed() << std::endl;
+
+        }
+
+        {
+            Timer all;
+            Timer  t;
+            task_work<bytes, dev::brc::Transaction>  tw(6);
+            for(auto count = 1; count < 100; count++){
+                t.restart();
+                std::vector<dev::brc::Transaction> ret;
+                tw.add_task(vec_trx, ret, [](const bytes &b){
+                    return dev::brc::Transaction(b, dev::brc::CheckTransaction::Everything);
+                });
+
+//                std::cout << "t : " << count << "  " << t.elapsed() << std::endl;
+
+//                BOOST_CHECK(ret1 == ret);
+            }
+            std::cout << "all : " << all.elapsed() << std::endl;
         }
 
 
