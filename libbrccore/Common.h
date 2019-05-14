9f62a17574217b7011d680088451486e821dc2a7
magicbaer <magicbaer@baerchain.net>
2019-05-14 14:38:00 +0800

    contract can init value , and transfer BRC. TODO remove log

--- libbrccore/Common.h
+++ libbrccore/Common.h
@@ -29,7 +29,7 @@ extern const unsigned c_databaseVersion;
 /// Address of the special contract for block hash storage defined in EIP96
 extern const Address c_blockhashContractAddress;
 /// Code of the special contract for block hash storage defined in EIP96
-extern const bytes c_blockhashContractCode;
+//extern const bytes c_blockhashContractCode;
 
 /// User-friendly string representation of the amount _b in wei.
 std::string formatBalance(bigint const& _b);
