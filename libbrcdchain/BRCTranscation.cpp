28aa7c4a4827b5848a1d6ef120494b365f3e87be
qwerbaer <qwerbaer@baerchain.net>
2019-05-30 14:24:18 +0800

    add verify pendingorder

--- libbrcdchain/BRCTranscation.cpp
+++ libbrcdchain/BRCTranscation.cpp
@@ -45,8 +45,10 @@ void dev::brc::BRCTranscation::verifyPendingOrder(Address const& _form, ex::exch
     {
          BOOST_THROW_EXCEPTION(VerifyPendingOrderFiled() << errinfo_comment(std::string("Pending order type and parameters are incorrect")));
     }
-
-	if( (_token_type != ex::order_token_type::BRC && _token_type != ex::order_token_type::FUEL) || (_buy_type != ex::order_buy_type::all_price && _buy_type != ex::order_buy_type::only_price))
+ 
+	if( (_type != ex::order_type::sell && _type != ex::order_type::buy) || 
+		(_token_type != ex::order_token_type::BRC && _token_type != ex::order_token_type::FUEL) || 
+		(_buy_type != ex::order_buy_type::all_price && _buy_type != ex::order_buy_type::only_price))
 	{
 		BOOST_THROW_EXCEPTION(VerifyPendingOrderFiled() << errinfo_comment(std::string("Pending order type and parameters are incorrect")));
 	} 
