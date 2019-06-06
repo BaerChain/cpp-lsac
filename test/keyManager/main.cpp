d768413cdc39794ac5cb9a02f1621883929933cd
qwerbaer <qwerbaer@baerchain.net>
2019-06-06 14:58:30 +0800

    add signatureTest

---
+++ test/keyManager/main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+
+#include <libdevcore/CommonIO.h>
+#include <libdevcore/FileSystem.h>
+#include <libdevcore/Common.h>
+#include <libdevcore/RLP.h>
+
+#include <libp2p/Host.h>
+
+#include <libdevcrypto/Common.h>
+#include <libdevcrypto/base58.h>
+
+#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
+
+namespace fs = boost::filesystem;
+namespace bop = boost::program_options;
+using namespace dev;
+
+KeyPair networkAlias(bytesConstRef _b)
+{
+    RLP r(_b);
+    if (r.itemCount() == 3 && r[0].isInt() && r[0].toInt<unsigned>() >= 3)
+        return KeyPair(Secret(r[1].toBytes()));
+    else
+        return KeyPair::create();
+}
+
+
+
+int main(int argc, char *argv[])
+{
+    bop::options_description _description("command line ");
+    _description.add_options()
+        ("data,d", bop::value<std::string>(), "data path");
+    
+    bop::variables_map args_map;
+    bop::parsed_options parsed = bop::parse_command_line(argc, argv, _description);
+    bop::store(parsed, args_map);
+    if(args_map.count("data"))
+    {
+        setDataDir(args_map["data"].as<std::string>());
+    }
+
+
+    auto netStates = contents(getDataDir() / fs::path("network.rlp"));
+
+    KeyPair _key =  networkAlias(&netStates);
+
+    std::cout << " public_key: " << _key.pub() << std::endl;
+    std::cout << " address :" << _key.address() << std::endl;
+    std::cout << " private_key: " << crypto::to_base58((char *)_key.secret().data(),32) << std::endl;
+
+
+    std::string _nodeid = "6c3f9db0041317229d4d517bcdd34b3517002a323e1a3a3b7483985c6f7702ad4e2fe8e3c848adaacb9f11d62b42646aafef654833519e01bf714debda27368e";
+    std::string _blockNum = "0x1";
+    std::string _blockHash = "0x48f683f4b75fca76a9c6a31c2a04fc3e1801d2078bd87e0f4234aacb620a8244";
+    std::string _version = "9d4ba962fb6addf7e02a15a57208138d0ac99ff5";
+    std::string _serverDelay = "40";
+    std::string _blockDelay = "40";
+    std::string _nodeConnectNum = "0x32";
+    std::string _minimumDelay = "40";
+    std::string _MaxDelay = "200";
+    std::string _packagedTransactions = "0x100";
+    std::string _tradingpoolsNum = "0x100";
+
+
+    RLPStream _rlp;
+    _rlp.appendList(11);
+    _rlp << _nodeid << _blockNum << _blockHash << _version << _serverDelay << _blockDelay << _nodeConnectNum << _minimumDelay << _MaxDelay << 
+        _packagedTransactions << _tradingpoolsNum;
+    
+    std::cout << "_rlp:" << _rlp.out() << std::endl;
+
+    Signature _signature = dev::sign(_key.secret(), sha3(_rlp.out()));
+
+    std::cout << "signature :" <<_signature << std::endl;
+
+    if(dev::verify(_key.pub(), _signature, sha3(_rlp.out())))
+    {
+        std::cout << "verify success" << std::endl;
+    }else{
+        std::cout << "verify error" << std::endl;
+    }
+
+}
