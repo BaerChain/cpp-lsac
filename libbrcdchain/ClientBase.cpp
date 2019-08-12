03ce4b2119e577c7aae0c7cfbea7d410568b1959
timobaer <timobaer@baerchain.net>
2019-08-12 16:53:09 +0800

    delete the Invalid and error interfaces

--- libbrcdchain/ClientBase.cpp
+++ libbrcdchain/ClientBase.cpp
@@ -338,7 +338,9 @@ LocalisedLogEntries ClientBase::checkWatch(unsigned _watchId)
     auto& w = m_watches.at(_watchId);
     //	LOG(m_loggerWatch) << "lastPoll updated to " <<
     // chrono::duration_cast<chrono::seconds>(chrono::system_clock::now().time_since_epoch()).count();
-    std::swap(ret, w.changes);
+    //std::swap(ret, w.changes);
+    ret = w.changes;
+    w.changes.clear();
     if (w.lastPoll != chrono::system_clock::time_point::max())
         w.lastPoll = chrono::system_clock::now();
 
