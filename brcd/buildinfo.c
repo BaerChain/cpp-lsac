0164e88fbf0cd96991d5c6a08796a541fb6ee719
magicbaer <magicbaer@baerchain.net>
2019-10-10 15:41:34 +0800

    v0.9.8

--- brcd/buildinfo.c
+++ brcd/buildinfo.c
@@ -14,7 +14,7 @@ const struct buildinfo* brcd_get_buildinfo()
 {
     static const struct buildinfo buildinfo = {
         .project_name = "brcd",
-        .project_version = "0.9.7",
+        .project_version = "0.9.8",
         .git_commit_hash = GIT_VERSION,
         .system_name = SYSTEM_NAME,
         .system_processor = SYSTEM_PROCESSOR,
