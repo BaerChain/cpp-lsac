fd8b178e946e9989064c18282bcb20ab1d0c3f8b
magicbaer <magicbaer@baerchain.net>
2021-01-27 17:05:48 +0800

    version 1.5.1

--- brcd/buildinfo.c
+++ brcd/buildinfo.c
@@ -14,7 +14,7 @@ const struct buildinfo* brcd_get_buildinfo()
 {
     static const struct buildinfo buildinfo = {
         .project_name = "brcd",
-        .project_version = "1.5.2", 
+        .project_version = "1.5.1", 
         .git_commit_hash = GIT_VERSION, 
         .system_name = SYSTEM_NAME,
         .system_processor = SYSTEM_PROCESSOR,
